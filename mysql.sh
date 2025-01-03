0b87d44c62151fd035db06f97b737949b882bd0d
JAYACHANDRA KUMAR <jayachandrakumar21@gmil.com>
2025-01-03 22:51:23 +0530

    Expense bash-scripting

--- mysql.sh
+++ mysql.sh
@@ -14,7 +14,7 @@ Y="\e[33m" #yellow
 N="\e[0m" #Default color
 
 # Redirctores
-LOGS_FOLDER="/var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/expense-bash-logs"
 LOG_FILE="$(echo $0 | cut -d "." -f1)"
 TIMESTAMP="$(date +%Y-%m-%d-%H-%M-%S)"
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
@@ -29,6 +29,8 @@ validation(){
         echo -e "$2... $G Success $N"
     fi
 }
+
+mkdir -p $LOGS_FOLDER
 
 echo "Script started Executing at :$TIMESTAMP" &>>$LOG_FILE_NAME
 
